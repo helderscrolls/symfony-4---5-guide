5b8983f29385d6086b26af4e0ef38f9f1a3bcfc6
Helder Nascimento <helder@lifeaz.fr>
2020-02-18 17:07:54 +0100

    feature(routes): add routing with annotations

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
+    /**
+     * @Route("/default", name="default")
+     */
     public function index()
     {
-        return new Response('<p> Hello! </p>');
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
     }
 }
