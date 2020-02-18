b251d1432fddc9353b5c10b3453da43fca3433d2
Helder Nascimento <helder@lifeaz.fr>
2020-02-18 17:05:18 +0100

    feature (routes): add other way of doing routes (without annotations)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends AbstractController
+class DefaultController
 {
-    /**
-     * @Route("/default", name="default")
-     */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return new Response('<p> Hello! </p>');
     }
 }
